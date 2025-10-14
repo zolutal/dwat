28c416ab47733703e9417f4cf1c25ecdbbf37e32
zolutal <justalmill@gmail.com>
2025-10-13 21:29:22 -0700

    update python bindings type hints, use 3.10 for building docs

--- python_stubs/dwat/__init__.pyi
+++ python_stubs/dwat/__init__.pyi
@@ -10,126 +10,76 @@ def load_dwarf_path(
 ) -> Dwarf: ...
 
 class Dwarf:
-    def lookup_type(self, named_type: NamedType, name: str) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base |
-        Const | Volatile | Restrict | Variable
-    ]: ...
+    def lookup_type(self, named_type: NamedType, name: str) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict | Variable: ...
 
-    def get_named_types_dict(self, named_type: NamedType) -> typing.Dict[
-        str,
-        typing.Union[
-            Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-            Base | Const | Volatile | Restrict | Variable
-        ]
-    ]: ...
+    def get_named_types_dict(self, named_type: NamedType) -> dict[str, Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict | Variable]: ...
 
-    def get_named_types(self, named_type: NamedType) -> typing.List[
-        typing.Tuple[
-            str,
-            Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-            Base | Const | Volatile | Restrict | Variable
-        ]
-    ]: ...
+    def get_named_types(self, named_type: NamedType) -> list[tuple[str, Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict | Variable]]: ...
 
 class Struct:
-    def members(self) -> typing.List[dwat.Member]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def members(self) -> list[dwat.Member]: ...
+    byte_size: int | None
+    name: str | None
 
 class Array:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
     bounds: int
 
 class Enum:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
 
 class Pointer:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    def deref(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    def deref(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
 
 class Subroutine:
-    def return_type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    def params(self) -> typing.List[Parameter]: ...
+    def return_type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    def params(self) -> list[Parameter]: ...
 
 class Typedef:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
 
 class Union:
-    def members(self) -> typing.List[dwat.Member]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def members(self) -> list[dwat.Member]: ...
+    byte_size: int | None
+    name: str | None
 
 class Base:
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    byte_size: int | None
+    name: str | None
 
 class Const:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
 
 class Volatile:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
 
 class Restrict:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
 
 class Member:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    byte_size: typing.Optional[int]
-    bit_size: typing.Optional[int]
-    offset: typing.Optional[str]
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    bit_size: int | None
+    offset: str | None
+    name: str | None
 
 class Parameter:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
 
 class Variable:
-    def type(self) -> typing.Union[
-        Struct | Array | Enum | Pointer | Subroutine | Typedef | Union |
-        Base | Const | Volatile | Restrict
-    ]: ...
-    name: typing.Optional[str]
+    def type(self) -> Struct | Array | Enum | Pointer | Subroutine | Typedef | Union | Base | Const | Volatile | Restrict: ...
+    byte_size: int | None
+    name: str | None
